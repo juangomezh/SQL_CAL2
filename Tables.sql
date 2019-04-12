bb450146996f8b9fab06a486a77cac7c6581434f
juangomezh <38103170+juangomezh@users.noreply.github.com>
2019-04-12 18:15:50 +0200

    Add files via upload

--- Tables.sql
+++ Tables.sql
@@ -1,19 +1,23 @@
-drop table if exists tickets;
 drop table if exists record;
 drop table if exists plays;
+drop table if exists gives;
+drop table if exists aboutdisks;
+drop table if exists aboutconcerts;
 drop table if exists opinions;
-drop table if exists user;
 drop table if exists members;
-drop table if exists buy;
+drop table if exists buytickets;
+drop table if exists buydisks;
 drop table if exists perform;
 drop table if exists musicgroup;
 drop table if exists song;
 drop table if exists instrument;
-drop table if exists contains;
-drop table if exists Disks;
+drop table if exists has;
 drop table if exists composes;
-drop table if exists musician;
 drop table if exists concerts;
+drop table if exists musician;
+drop table if exists tickets;
+drop table if exists customer;
+drop table if exists Disks;
 
 create table musician
 (	name varchar(10),
@@ -58,6 +62,7 @@ create table concerts
 );
 create table Disks
 (	Referencenumber numeric(9,0),
+	format		varchar(10),
 	title	varchar(10),
     year numeric(4,0),
     month numeric(4,0),
@@ -65,7 +70,8 @@ create table Disks
     genre varchar(10),
     primary key (Referencenumber)
 );
-create table user
+
+create table customer
 (	name varchar(10),
 	email varchar(10),
     points int,
@@ -74,18 +80,18 @@ create table user
 	primary key (email)
 );
 create table tickets
-(	code	numeric(9,0),
+(	ticketcode	numeric(9,0),
 	year numeric(4,0),
     month numeric(4,0),
     day	  numeric(2,0),
     price int,
-    primary key (code)
+    primary key (ticketcode)
 );
 create table opinions
-(	description varchar(500),
+(	opdescription varchar(500),
 	email varchar(10),
-    primary key (email),
-    foreign key (email) references user(email)
+    primary key (email, opdescription),
+    foreign key (email) references customer(email)
 );
 create table plays
 (	ID	char(9),
@@ -101,7 +107,7 @@ create table composes
     primary key(ID),
     foreign key(ID) references musician(ID)
 );
-create table contains
+create table has
 (	Referencenumber numeric(9,0),
 	duration		int,
     year numeric(4,0),
@@ -120,13 +126,6 @@ create table members
     foreign key(ID) references musician(ID),
     foreign key(groupID) references musicgroup(groupID)
     );
-create table buy
-(	ID char(9),
-	Referencenumber	numeric(9,0),
-    primary key(ID, Referencenumber),
-    foreign key(ID) references musician(ID),
-    foreign key(Referencenumber) references Disks(Referencenumber)
-    );
 create table perform 
 (	groupID		char(9),
 	Code	char(9),
@@ -141,7 +140,39 @@ create table record
     foreign key(groupID) references musicgroup(groupID),
     foreign key(Referencenumber) references disks(Referencenumber)
     );
-    
+create table buytickets
+(	ticketcode	numeric(9,0),
+	email varchar(10),
+	primary key(ticketcode, email),
+    foreign key(ticketcode) references tickets(ticketcode),
+    foreign key(email) references customer(email)
+);
+create table buydisks
+(
+	Referencenumber	 numeric(9,0),
+	email varchar(10),
+	primary key(Referencenumber, email),
+    foreign key(Referencenumber) references Disks(Referencenumber),
+    foreign key(email) references customer(email)
+);
+create table aboutdisks
+(
+	Referencenumber numeric(9,0),
+	opdescription	varchar(500),
+    email	varchar(10),
+	primary key(Referencenumber, email),
+    foreign key(Referencenumber) references Disks(Referencenumber),
+    foreign key(email) references opinions(email)
+);
+create table aboutconcerts
+(
+	Code	char(9),
+	opdescription	varchar(500),
+	email varchar(10),
+	primary key(Code, email),
+    foreign key(Code) references concerts(Code),
+    foreign key(email) references opinions(email)
+);
 
 
 
